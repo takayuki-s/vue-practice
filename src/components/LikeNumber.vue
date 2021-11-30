d421d04fe309fdc187961a18bd2bcd54a38bbd22
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-01 08:23:12 +0900

    propsのバリデーション
    requireとdefaultは対になっている。

--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -7,7 +7,12 @@
 
 <script>
 export default {
-  props: ["totalNumber"],
+  props: {
+    totalNumber: {
+      type: Number,
+      required: true,
+    }
+  },
   computed: {
     halfNumber() {
       return this.totalNumber /2
