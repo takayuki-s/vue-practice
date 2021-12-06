ea415c1b1c2bc8097c0e716f56ade17c54415d96
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-07 07:25:43 +0900

    trim修飾子
    trim修飾子をつけることによって、先頭と最後の空白を省略する。
    
    ※打ち込んだ通りに空白の適用を行うように、pタグをpreタグに変換している

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,13 @@
       <h2>イベントのフォーム</h2>
       <label for="title">タイトル</label>
       <input id="title" type="text" v-model.lazy="eventData.title">
-      <p>{{ eventData.title }}</p>
+      <pre>{{ eventData.title }}</pre>
       <label for="maxNumber">最大人数</label>
       <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
       <p>{{ typeof eventData.maxNumber }}</p>
+      <label for="host">主催者</label>
+      <input id="host" type="text" v-model.trim="eventData.host">
+      <pre>{{ eventData.host }}</pre>
     </div>
   </div>
 </template>
@@ -35,6 +38,7 @@ export default {
       eventData: {
         title: "タイトル",
         maxNumber: 0,
+        host: "",
       }
     }
   },
