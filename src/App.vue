0214574c1c109bd5af5099a613b3a86a358f8c11
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-08 08:31:04 +0900

    ラジオボタンに双方向データバインディング

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,11 @@
       <input type="checkbox" id="30" value="30代" v-model="eventData.target">
       <label for="30">30代</label>
       <p>{{ eventData.target }}</p>
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
     </div>
   </div>
 </template>
@@ -56,6 +61,7 @@ export default {
         detail: "",
         isPrivate: false,
         target: [],
+        price: "無料",
       }
     }
   },
