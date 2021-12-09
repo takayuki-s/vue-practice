997e34327113d57a32edd5af52cd6a186115eac6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-10 07:15:51 +0900

    カスタムディレクティブの省略記法
    bindとupdateは同じコードを書くことが多いので、省略して関数で書くことができる

--- src/main.js
+++ src/main.js
@@ -4,23 +4,7 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
-Vue.directive("border", {
-  bind(el, binding, vnode) {
-    // ディレクティブが初めて対象の要素にひもづいた時
-  },
-  inserted(el, binding, vnode) {
-    // 親Nodeに挿入された時
-  },
-  update(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される前
-  },
-  componentUpdated(el, binding, vnode, oldVnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される後
-  },
-  unbind(el, binding, vnode) {
-    // ディレクティブがひもづいている要素から取り除かれた時
-  },
-});
+Vue.directive("border", function (el, binding) {});
 
 new Vue({
   render: (h) => h(App),
