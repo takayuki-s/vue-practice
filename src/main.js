45cb9d1dd0d24242249dc80804f910dde06f8323
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-10 07:21:40 +0900

    elを使って直接DOM操作
    elはそのカスタムディレクティブがひもづく要素を表している（今回はv-borderのあるp要素）

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
-Vue.directive("border", function (el, binding) {});
+Vue.directive("border", function (el) {
+  el.style.border = "solid black 2px";
+});
 
 new Vue({
   render: (h) => h(App),
