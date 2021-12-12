f472492a038fbd42661b4c497875253f895bf219
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-13 08:46:32 +0900

    修飾子をカスタムディレクティブに対して使用

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,3 @@
 <template>
-  <p v-border:dotted="{width: '3px', color: 'red'}">Home</p>
+  <p v-border:solid.round="{width: '3px', color: 'red'}">Home</p>
 </template>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ Vue.directive("border", function (el, binding) {
   el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
   el.style.borderStyle = binding.arg;
+  if (binding.modifiers.round) {
+    el.style.borderRadius = "0.5rem";
+  }
 });
 
 new Vue({
