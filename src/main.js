40a6a9804fda81343bd3a54b2893fe57d90fbe07
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-13 08:54:56 +0900

    ローカルにカスタムディレクティブを登録する
    directivesオプションを使う

--- src/main.js
+++ src/main.js
@@ -4,17 +4,6 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
-Vue.directive("border", function (el, binding) {
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if (binding.modifiers.round) {
-    el.style.borderRadius = "0.5rem";
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = "0px 2px 5px rgba(0, 0, 0, 0.26)";
-  }
-});
 
 new Vue({
   render: (h) => h(App),
