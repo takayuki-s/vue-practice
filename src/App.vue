9628da38b297b1101100ccc019d891f699e0eb51
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-16 08:59:40 +0900

    fadeするトランジション効果をCSSトランジションを使って実際に作成

--- src/App.vue
+++ src/App.vue
@@ -91,21 +91,27 @@ export default {
 <style scoped>
 .fade-enter {
   /* 現れる時の最初の状態 */
+  opacity: 0;
 }
 .fade-enter-active {
   /* 現れる時のトランジションの状態 */
+  transition: opacity .5s;
 }
 .fade-enter-to {
   /* 現れる時の最後の状態 */
+  opacity: 1;
 }
 .fade-leave {
   /* 消える時の最初の状態 */
+  opacity: 1;
 }
 .fade-leave-active {
   /* 消える時のトランジションの状態 */
+  transition: opacity .5s;
 }
 .fade-leave-to {
   /* 消える時の最後の状態 */
+  opacity: 0;
 }
 
 .main {
