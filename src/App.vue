08f4c0cb7383b3cf86c511ffc2acdc4903291464
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-18 06:54:47 +0900

    要素間のトランジションのタイミングを整える
    modeにはout-inとin-outがある。out-inがよく使われる。

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <button @click="myAnimation = 'fade'">Fade</button>
       <p>{{ myAnimation }}</p>
       <button @click="show = !show">切り替え</button>
-      <transition name="fade">
+      <transition name="fade" mode="out-in">
         <p v-if="show" key="bye">さよなら</p>
         <p v-else key="hello">こんにちは</p>
       </transition>
