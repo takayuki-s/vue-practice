8d0dcd66f7813a7397182925eb8fad789e0ee2f3
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-20 06:43:19 +0900

    JavaScriptフックを使用してアニメーションを作成
    enter、leave、（beforeEnter）の3つが大体使われる

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,7 @@
         :css="false"
         @before-enter="beforeEnter"
         @enter="enter"
-        @after-enter="afterEnter"
-        @enter-cancelled="enterCancelled"
-        @before-leave="beforeLeave"
         @leave="leave"
-        @after-leave="afterLeave"
-        @leave-cancelled="leaveCancelled"
-
       >
         <div class="circle" v-if="show"></div>
       </transition>
@@ -126,27 +120,31 @@ export default {
   methods: {
     beforeEnter(el) {
       // 現れる前
+      el.style.transform = `scale(0)`
     },
     enter(el, done) {
       // 現れる時
-    },
-    afterEnter(el) {
-      // 現れた後
-    },
-    enterCancelled(el) {
-      // 現れるアニメーションがキャンセルされた時
-    },
-    beforeLeave(el) {
-      // 消える前
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20)
     },
     leave(el, done) {
       // 消える時
-    },
-    afterLeave(el) {
-      // 消えた後
-    },
-    leaveCancelled(el) {
-      // 消えるアニメーションがキャンセルされた時
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20)
     },
   },
   components: {
