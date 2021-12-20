b9db9318a05f2b9ea85c58736e6f65652e51722c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-21 08:06:13 +0900

    VueRouterを使ったルーティング

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
 import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
@@ -15,5 +16,6 @@ Vue.mixin({
 });
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
