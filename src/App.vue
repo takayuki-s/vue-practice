95abf62a552591224e315bbbdb29d517f28de665
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-22 07:46:45 +0900

    通常の画面遷移

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <a href="/">Home</a>
+      <a href="/users">Users</a>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
