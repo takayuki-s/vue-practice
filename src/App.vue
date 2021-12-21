6be45de20ac495d04c5858ef78dce755279b1373
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-22 08:15:47 +0900

    active-link属性、exact属性
    active-link→そのURLから始まる限りはずっとactive、包含的
    exact→完全一致

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
     <nav>
-      <router-link to="/" class="link">Home</router-link>
-      <router-link to="/users" class="link">Users</router-link>
+      <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
+      <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -11,5 +11,8 @@
 <style scoped>
 .link {
   margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
 }
 </style>
