445f910333234ac4a1518bea4307cb3c5d6d5f05
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-23 08:03:51 +0900

    URLパラメータ取得
    コロン(:)で動的なURLを作る
    
    routeオブジェクトを利用してパラメーターを取得

--- src/router.js
+++ src/router.js
@@ -11,6 +11,6 @@ export default new Router({
   // URLとコンポーネントをマッピング
   routes: [
     { path: "/", component: Home },
-    { path: "/users", component: Users },
+    { path: "/users/:id", component: Users },
   ],
 });
