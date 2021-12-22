2fd34329913959eeb7dbb75c9eea6de0e9fc341f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-23 08:57:39 +0900

    パラメーターが変わってもライフサイクルメソッドは呼ばれない
    この場合logにcreated!と表示されるのは最初の1回だけ

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <h3>Users</h3>
+    <router-link to="/users/1">ユーザー1</router-link>
+    <router-link to="/users/2">ユーザー2</router-link>
     <hr>
     <h1>User No. {{ $route.params.id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    console.log('created!');
+  }
+}
+</script>
