ad8d874b1e989cbbe51802f4d390785128472550
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-12-28 07:42:00 +0900

    URLにハッシュ
    vue.jsではデフォルトではハッシュ遷移が効かない
    
    vue-routerの振る舞いをこっちで決めないといけない

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -5,9 +5,10 @@
     <router-link to="/users/2">ユーザー2</router-link>
     <hr>
     <h1>User No. {{ $route.params.id }}</h1>
-    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
-    <router-link :to="{name: 'users-id-profile', params: {id: Number(id) + 1}, query: { lang: 'ja', page: 2}}">次のユーザー</router-link>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja#next-user'">次のユーザー</router-link>
     <router-view></router-view>
+    <div style="height: 700px;"></div>
+    <router-link id="next-user" :to="{name: 'users-id-profile', params: {id: Number(id) + 1}, query: { lang: 'ja', page: 2}, hash: '#next-user'}">次のユーザー</router-link>
   </div>
 </template>
 
