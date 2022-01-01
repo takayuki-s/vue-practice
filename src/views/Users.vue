8acd65ba5b59413d068963c6e89098487b1dbf93
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-01 09:34:45 +0900

    コンポーネントに指定できる３つのナビゲーションガード
    boforeRouteEnter→コンポーネントが実行される時の処理
    
    boforeRouteUpdate→URLが変わったけど、ビューインスタンスに変更がない時の処理
    
    beforeRouteLeave→ページが離れる時の処理

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -17,13 +17,23 @@
 export default {
   props: ["id"],
   beforeRouteEnter(to, from, next) {
-    next();
+    console.log("beforeRouteEnter")
+    next(vm => {
+      console.log(vm.id)
+    });
   },
   beforeRouteUpdate(to, from, next) {
+    console.log("beforeRouteUpdate")
     next();
   },
   beforeRouteLeave(to, from, next) {
-    next();
+    console.log("beforeRouteLeave")
+    const isLeave = window.confirm("本当にこのページを離れますか？")
+    if (isLeave) {
+      next()
+    } else {
+      next(false)
+    }
   },
 
 }
