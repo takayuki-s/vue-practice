9fcc9368739dffaf20c493221f24db9285cb4b76
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-01 09:36:16 +0900

    ナビゲーションガードのコード削除

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -16,25 +16,5 @@
 <script>
 export default {
   props: ["id"],
-  beforeRouteEnter(to, from, next) {
-    console.log("beforeRouteEnter")
-    next(vm => {
-      console.log(vm.id)
-    });
-  },
-  beforeRouteUpdate(to, from, next) {
-    console.log("beforeRouteUpdate")
-    next();
-  },
-  beforeRouteLeave(to, from, next) {
-    console.log("beforeRouteLeave")
-    const isLeave = window.confirm("本当にこのページを離れますか？")
-    if (isLeave) {
-      next()
-    } else {
-      next(false)
-    }
-  },
-
 }
 </script>
