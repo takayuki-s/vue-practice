b31420e7b78b1d2efb96488c3850da3762b54687
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-04 08:39:35 +0900

    mapMutaionsヘルパーで、効率よくmutationsをコンポーネントに追加

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,20 +3,16 @@
     <h2>Home</h2>
     <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
     <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
-    <button @click="increment">+1</button>
-    <button @click="decrement">-1</button>
+    <button @click="increment(1)">+1</button>
+    <button @click="decrement(1)">-1</button>
   </nav>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
   methods: {
-    increment() {
-      this.$store.commit('increment', 1);
-    },
-    decrement() {
-      this.$store.commit('decrement', 1);
-    }
+    ...mapMutations(["increment", "decrement"])
   }
 }
 </script>
