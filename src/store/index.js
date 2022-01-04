f14c9a98527ea488491629699b9072442e2c0628
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-05 06:45:45 +0900

    index.jsのカウント機能をモジュールとして切り出す

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import count from "./modules/count";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
-    count: 2,
     message: "",
   },
   getters: {
-    doubleCount: (state) => state.count * 2,
     message: (state) => state.message,
   },
   mutations: {
-    increment(state, number) {
-      state.count += number;
-    },
-    decrement(state, number) {
-      state.count -= number;
-    },
     updateMessage(state, newMessage) {
       state.message = newMessage;
     },
   },
   actions: {
-    increment({ commit }, number) {
-      commit("increment", number);
-    },
-    decrement({ commit }, number) {
-      commit("decrement", number);
-    },
     updateMessage({ commit }, newMessage) {
       commit("updateMessage", newMessage);
     },
+  },
+  modules: {
+    count,
   },
 });
---
+++ src/store/modules/count.js
@@ -0,0 +1,32 @@
+const state = {
+  count: 2,
+};
+
+const getters = {
+  doubleCount: (state) => state.count * 2,
+};
+
+const mutations = {
+  increment(state, number) {
+    state.count += number;
+  },
+  decrement(state, number) {
+    state.count -= number;
+  },
+};
+
+const actions = {
+  increment({ commit }, number) {
+    commit("increment", number);
+  },
+  decrement({ commit }, number) {
+    commit("decrement", number);
+  },
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+};
