b7eab4c9cbd72bdce8f5aa76c2372d94a43d1fdb
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-06 08:39:43 +0900

    掲示板の入力フォーム作成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
     <router-view name="header"></router-view>
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム：</label>
+    <input id="name" type="text" v-model="name">
+    <br><br>
+    <label for="comment">コメント：</label>
+    <textarea id="comment" v-model="comment"></textarea>
+    <h2>掲示板</h2>
     <transition name="fade" mode="out-in" @before-enter="beforeEnter">
       <router-view></router-view>
     </transition>
@@ -9,6 +16,12 @@
 
 <script>
 export default {
+  data() {
+    return {
+      name: "",
+      comment: "",
+    };
+  },
   methods: {
     beforeEnter() {
       this.$root.$emit('triggerScroll')
