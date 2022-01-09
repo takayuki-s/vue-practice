7a6e4a672cab629885095d2b3304e5faeddda475
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-09 22:31:42 +0900

    ヘッダーとrouter-view準備

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <header>
+      <router-link to="/" class="header-item">掲示板</router-link>
+      <router-link to="/login" class="header-item">ログイン</router-link>
+      <router-link to="/register" class="header-item">登録</router-link>
+    </header>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -51,4 +57,7 @@ export default {
 </script>
 
 <style scoped>
+.header-item {
+  padding: 10px;
+}
 </style>
