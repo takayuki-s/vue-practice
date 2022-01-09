0d08d468f08718c2923846fc6938e7e1ee5bee95
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-10 07:17:09 +0900

    ログインビュー

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <router-view name="header"></router-view>
+    <h2>ログイン</h2>
+    <label for="email">Email：</label>
+    <input id="email" type="email" v-model="email">
+    <br><br>
+    <label for="password">パスワード：</label>
+    <input id="password" type="password" v-model="password">
+    <br><br>
+    <button @click="login">送信</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      alert("login")
+    }
+  }
+}
+</script>
