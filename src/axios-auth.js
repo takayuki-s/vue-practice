cd4915534bbb4e793e906c5d0a4e67dd9d1c0ed0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-10 07:41:57 +0900

    baseURLを変更
    https://firebase.google.com/docs/reference/rest/auth#section-create-email-password

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    "https://firestore.googleapis.com/v1/projects/vuejs-http-d048c/databases/(default)/documents",
+  // baseURL:
+  //   "https://firestore.googleapis.com/v1/projects/vuejs-http-d048c/databases/(default)/documents",
+  baseURL: "https://identitytoolkit.googleapis.com/v1/",
 });
 
 export default instance;
