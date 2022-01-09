0f02f490fca2373af1067e8d4e809d20d5204af0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-10 07:21:51 +0900

    新規登録ビュー

---
+++ src/views/Resister.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <router-view name="header"></router-view>
+    <h2>登録</h2>
+    <label for="email">Email：</label>
+    <input id="email" type="email" v-model="email">
+    <br><br>
+    <label for="password">パスワード：</label>
+    <input id="password" type="password" v-model="password">
+    <br><br>
+    <button @click="resister">送信</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    resister() {
+      alert("login")
+    }
+  }
+}
+</script>
