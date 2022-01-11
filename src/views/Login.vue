c17c5b3b601a6af428ff959cc8cd26ac21dae5ed
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-11 09:26:09 +0900

    ログイン機能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,12 +7,12 @@
     <label for="password">パスワード：</label>
     <input id="password" type="password" v-model="password">
     <br><br>
-    <button @click="login">送信</button>
+    <button @click="resister">送信</button>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "../axios-auth";
 export default {
   data() {
     return {
@@ -21,8 +21,17 @@ export default {
     };
   },
   methods: {
-    login() {
-      alert("login")
+    resister() {
+      axios.post(
+        `accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_API_KEY}`,
+        {
+          email: this.email,
+          password: this.password,
+          returnSecureToken: true,
+        }
+      ).then(response => {
+        console.log(response);
+      })
     }
   }
 }
