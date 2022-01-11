039ba4eef34fd234064b7c3f71de36e67499fec7
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-01-12 08:44:18 +0900

    ログイン時にトークン取得

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,36 @@ import Vue from "vue";
 import Vuex from "vuex";
 import count from "./modules/count";
 import getters from "./getters";
-import mutations from "./mutations";
-import actions from "./actions";
+import axios from "../axios-auth";
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     message: "",
+    idToken: null,
   },
   getters,
-  mutations,
-  actions,
+  mutations: {
+    updateIdToken(state, idToken) {
+      state.idToken = idToken;
+    },
+  },
+  actions: {
+    login({ commit }, authData) {
+      axios
+        .post(
+          `accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_API_KEY}`,
+          {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true,
+          }
+        )
+        .then((response) => {
+          commit("updateIdToken", response.data.idToken);
+        });
+    },
+  },
   modules: {
     count,
   },
